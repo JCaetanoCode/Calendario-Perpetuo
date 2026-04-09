4f293d9218bd87cd7b760602747f5b24d896549d
Jairo Caetano <46423193+JCaetanoCode@users.noreply.github.com>
2026-04-09 20:35:32 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("meu-app").then(function(cache) {
-      return cache.addAll([
-        "index.html"
-      ]);
-    })
-  );
-});
